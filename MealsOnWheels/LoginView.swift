fcf374b4c7d5c79b5c8eea488d42874ae67cb95d
Rahul Ajmera <rahulajmera@lawn-128-61-7-133.lawn.gatech.edu>
2016-10-06 14:57:47 -0400

    Worked on Login View/ Login Controller

--- MealsOnWheels/LoginView.swift
+++ MealsOnWheels/LoginView.swift
@@ -23,56 +23,115 @@ class LoginView: UIView {
     var signUpBtn = UIButton()
     var forgotPasswordBtn = UIButton()
     
+    
+    
     func configureImageViews() {
         logoImgView.image = MWConstants.titleImg
         logoImgView.contentMode = .scaleAspectFit
     }
     
     func configureTextFields() {
-        emailTF.placeholder = "Email"
+     // Setting text field item properties
+        emailTF.layer.cornerRadius = 10.0
+        emailTF.layer.borderWidth = 1
         
-        passwordTF.placeholder = "Password"
+        passwordTF.layer.cornerRadius = 10.0
+        passwordTF.layer.borderWidth = 1
+        
+    // Setting text properties
+        
+        emailTF.placeholder = " Email"
+        emailTF.font = UIFont(name: "Avenir-Medium", size: 18.0)
+        emailTF.textColor = UIColor.white
+        //emailTF.textAlignment = NSTextAlignment.center
+        
+        
+        passwordTF.placeholder = " Password"
+        passwordTF.font = UIFont(name: "Avenir-Medium", size: 18.0)
+        passwordTF.textColor = UIColor.white
+        passwordTF.isSecureTextEntry = true
+        //passwordTF.textAlignment = NSTextAlignment.center
+        
+        
+    }
+    
+    
+    func textFieldDidBeginEditing(_ textField : UITextField)
+    {
+        emailTF.autocorrectionType = .no
+        emailTF.autocapitalizationType = .none
+        emailTF.spellCheckingType = .no
     }
     
     func configureButtons() {
-        loginBtn.setTitle("Login", for: UIControlState())
-        loginBtn.setTitleColor(UIColor.blue, for: UIControlState())
-        loginBtn.backgroundColor = UIColor.white
         
-        signUpBtn.setTitle("Sign Up", for: UIControlState())
-        signUpBtn.setTitleColor(UIColor.blue, for: UIControlState())
-        signUpBtn.backgroundColor = UIColor.white
+        
+        loginBtn.setTitle("Login", for: UIControlState())
+        loginBtn.setTitleColor(UIColor.darkText, for: UIControlState())
+        loginBtn.backgroundColor = UIColor.white
+        loginBtn.layer.cornerRadius = 20.0
+        loginBtn.layer.borderColor = UIColor.white.cgColor
+        loginBtn.layer.borderWidth = 1
+        
+        
+        signUpBtn.setTitle("Need to Sign Up?", for: UIControlState())
+        signUpBtn.setTitleColor(UIColor.lightText, for: UIControlState())
+        signUpBtn.titleLabel?.font = UIFont(name: "Avenir-Medium", size: 12.0)
+        
+        forgotPasswordBtn.setTitle("Forgot Password?", for: UIControlState())
+        forgotPasswordBtn.setTitleColor(UIColor.lightText, for: UIControlState())
+        forgotPasswordBtn.titleLabel?.font = UIFont(name: "Avenir-Medium", size: 12.0)
+
+        
+        
     }
     
+    
+    
     func configureView() {
-        self.backgroundColor = MWConstants.colors.loginBackground
+        
+        let gradient = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = [MWConstants.colors.loginDarkGradient.cgColor, MWConstants.colors.loginLightGradient.cgColor]
+        self.layer.insertSublayer(gradient, at: 0)
         
         configureImageViews()
         configureTextFields()
         configureButtons()
+        textFieldDidBeginEditing(emailTF)
         
         //Auto Layout
         let viewsDict = [
-            "logo"  :   logoImgView,
+            "title"  :   logoImgView,
             "emTF"  :   emailTF,
             "psTF"  :   passwordTF,
             "login" :   loginBtn,
-            "signup":   signUpBtn
-        ] as [String : Any]
+            "signup":   signUpBtn,
+            "forgotPw": forgotPasswordBtn
+            ] as [String : UIView]
         
-        self.prepareViewsForAutoLayout(viewsDict as! [String : UIView])
+        self.prepareViewsForAutoLayout(viewsDict)
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-20-[logo]-20-[login]-20-[signup]", views: viewsDict as [String : AnyObject]))
+//          self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:[title]-100-[emTF]-30-[psTF]-60-[login]-30-[signup]-|", views: viewsDict as [String : AnyObject]))
+//        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("V:|-40-[title]-100-[emTF]-30-[psTF]-60-[login]-10-[forgotPw]-30-[signup]|", views: viewsDict))
+//
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|[title]|", views: viewsDict))
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[logo]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[emTF]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
-        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[login(==100)]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+//         self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-30-[title]-30-|", views: viewsDict as [String : AnyObject]))
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[psTF]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
+        
+        self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[login]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
         self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[signup]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
-
+         self.addConstraints(NSLayoutConstraint.constraintsWithSimpleFormat("H:|-\(String(describing: MWConstants.loginFieldsOffset))-[forgotPw]-\(String(describing: MWConstants.loginFieldsOffset))-|", views: viewsDict as [String : AnyObject]))
         
     }
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
