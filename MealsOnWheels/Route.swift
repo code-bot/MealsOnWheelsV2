fa303caba3eb514940c75d1831eecfe531e363f9
akhiaji <akhiaji@gmail.com>
2016-10-13 14:16:19 -0400

    added method to route for initiating next leg

--- MealsOnWheels/Route.swift
+++ MealsOnWheels/Route.swift
@@ -17,7 +17,7 @@ class Route {
     var totalMiles: Double
     var estimatedTime: String
     var overviewPolyline: GMSPath
-
+    var currentWaypoint: Waypoint?
     init(name: String, desc: String, waypoints: [Waypoint], miles: Double, time: String, overviewPolyline: String) {
         self.name = name
         self.description = desc
@@ -27,6 +27,18 @@ class Route {
         self.estimatedTime = time
         self.totalMiles = miles
         self.overviewPolyline = GMSPath(fromEncodedPath: overviewPolyline)!
+    }
+    
+    func nextLeg() {
+        self.currentWaypoint = waypoints.pop() as Waypoint!
+        let htmlDestination = currentWaypoint?.address.addingPercentEscapes(using: String.Encoding.utf8)!
+        let directionsRequest = "comgooglemaps-x-callback:" + "?daddr=" + htmlDestination! + "&x-success=MOWapp:?resume=true&x-source=MOW"
+        let directionsURL = URL(string:directionsRequest);
+        if (UIApplication.shared.canOpenURL( URL(string: "comgooglemaps-x-callback:")!)) {
+            UIApplication.shared.openURL(directionsURL!)
+        } else {
+            print("Can't use comgooglemaps:");
+        }
     }
     
 }
